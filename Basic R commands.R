31bef0c0413eac41406b8e7829ab98473a164f09
jhonalexis-parra <jhalpaab@gmail.com>
2019-12-13 21:14:50 -0500

    Course R-Basic, exercices 1.2

---
+++ Basic R commands.R
@@ -0,0 +1,9 @@
+#Formula cuadratica
+
+x1 <- (-b + sqrt(b^2 - 4*a*c )) / (2*a)
+x2 <- (-b - sqrt(b^2 - 4*a*c )) / (2*a)
+print("El valor de x1 es: ")
+print(x1)
+print("El valor de x2 es: ")
+print(x2)
+
